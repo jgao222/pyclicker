5eaafe239c869b4e392af3b92581059e7a4b0fd9
jgao2 <jgao222@uw.edu>
2022-07-09 17:38:20 -0700

    refactored slider ui element

--- gui/gui.py
+++ gui/gui.py
@@ -8,6 +8,7 @@ from tkinter.constants import VERTICAL
 # local imports
 import consts
 import gui.activity_label as activity_label
+import gui.slider as slider
 
 
 class MainGui(tk.Frame):
@@ -30,29 +31,11 @@ class MainGui(tk.Frame):
         self._event_listeners["active_change"].append(active_label.update)
         active_label.grid(row=0, column=0, columnspan=2, pady="0 20", sticky="nw")
 
-        self._CPS_TEXT = tk.StringVar(master=root, name="CPS_DISPLAY")
-        self._CPS_TEXT.set("CPS (Rate): " + str(consts.DEFAULT_CPS))
+        self._slider = slider.Slider(self)
+        self._slider.set_callback(lambda new_cps: self.emit_event("cps_change", new_cps))
+        self._slider.grid(row=2, column=0, rowspan=5, columnspan=2)
 
-        clicks_label = ttk.Label(self, textvariable=self._CPS_TEXT)
-        clicks_label.grid(row=1, column=0, columnspan=2)
-
-        self._CPS_SCALE_VALUE = tk.DoubleVar()
-        self._CPS_SCALE_VALUE.set(consts.DEFAULT_CPS)
-        slider = ttk.Scale(
-            self,
-            orient=VERTICAL,
-            length=500,
-            # make sure to configure callback for this in outside code
-            name="cps_adjust_slider",
-            from_=50.0,
-            to=1.0,
-            variable=self._CPS_SCALE_VALUE,
-            command=self.update_cps
-            # not supported on ttk.scale
-            #  tickinterval=10.0
-        )
-        slider.grid(row=2, column=0, rowspan=5, columnspan=2)
-
+        # grid self onto root
         self.grid(row=0, column=0, sticky="nsw", padx="15 15", pady="20 20")
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
@@ -64,13 +47,6 @@ class MainGui(tk.Frame):
         Please don't modify the map returned by this function!
         """
         return self._externally_visible_variables
-
-
-    def update_cps(self, new_cps):
-        cps_val = round(float(new_cps), consts.ROUND_PRECISION)
-        self._CPS_TEXT.set("CPS (Rate): " + str(cps_val))
-        # send external listeners message that the value was updated
-        self.emit_event("cps_change", cps_val)
 
 
     def emit_event(self, event, value):
